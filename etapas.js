680ef0d7633a6889ddfd9eaf07af306ad82fd0ce
Amanda Luiza <110351770+AmandaLuizaFreitas@users.noreply.github.com>
2023-03-05 14:52:17 -0300

    Add files via upload

---
+++ etapas.js
@@ -0,0 +1,73 @@
+let etapas = [
+ {
+  titulo:'VEREADOR',
+  numeros:5,
+  condidatos:[
+   {
+    numero:'38111',
+    nome:' Bob Sideshow',
+    partido:'MBT',
+    fotos:[
+     {
+      url:'bob.png',legenda:'Vereador', small:false
+     }
+    ]
+
+   },
+   {
+    numero:'77222',
+    nome:'Moe Szyslak',
+    partido:'BAR',
+    fotos:[
+     {
+      url:'moe.png',legenda:'Vereador'
+     }
+    ]
+   },
+   {
+      numero:'38845',
+    nome:'Cecil Terwigiller',
+    partido:'MBT',
+    fotos:[
+     {
+      url:'cecil.jpg',legenda:'Vereador'
+     }
+    ] 
+   }
+
+  ]
+ },
+ {
+  titulo:'PREFEITO',
+  numeros:2,
+  condidatos:[
+   {
+    numero:'99',
+    nome:'Joe Quimby',
+    partido:'MFA',
+    vice:'Tony Gordo',
+    fotos:[
+     {url:'joe.jpg',legenda:'Perfeito'},
+     {url:'tony.jpg',legenda:'Vice-Perfeito', small:true}],
+    },
+    {
+     numero:'74',
+      nome:'Seymour Skinny',
+      partido:'EDC',
+      fotos:[
+       {url:'seymour.png',legenda:'Perfeito'},
+      
+      ]
+   },
+   {
+      numero:'77',
+       nome:'Reverendo Lovejoy',
+       partido:'IGJ',
+       fotos:[
+        {url:'reverendo.png',legenda:'Perfeito'},
+       
+       ]
+    }
+  ]
+ }
+]
